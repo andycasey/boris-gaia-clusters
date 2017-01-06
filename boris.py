39fce0be489144e0be171fb6ec67cfc10a0deadf
Andy Casey <andycasey@gmail.com>
2017-01-06 17:23:58 +0000

    initial commit

---
+++ boris.py
@@ -0,0 +1,70 @@
+
+import yaml
+from astropy.table import Table
+
+import sqlutil
+from database import Database as WSDB
+
+
+# Load clusters.
+clusters = Table.read("clusters.txt", format="ascii")
+
+
+# Credentials
+with open("wsdb.yaml", "r") as fp:
+    credentials = yaml.load(fp)
+
+wsdb = WSDB(**credentials)
+
+cone = 1.0/3600 # degrees
+catalog_names = ("apassdr9.main", "twomass.psc", "unwise.sdss_forced")
+
+for cluster_name, ra, dec, radius, N in clusters:
+
+    print("Querying TGAS for {}".format(cluster_name))
+
+    kwds = dict(cluster_ra=ra, cluster_dec=dec, radius=radius, cone=cone)
+
+    # Query all TGAS within +/- radius.
+    tgas_sources = wsdb.retrieve_table(
+        """ SELECT *
+            FROM gaia_dr1.tgas_source
+            WHERE q3c_radial_query(ra, dec, %(cluster_ra)s, %(cluster_dec)s, %(radius)s)
+        """, kwds)
+    tgas_sources.write("{}-tgas.csv".format(cluster_name))
+
+    print("Found {N} TGAS sources for {cluster_name}".format(
+        N=len(tgas_sources), cluster_name=cluster_name))
+
+    # Cross-match TGAS with catalogs in the same radius.
+    for catalog_name in catalog_names:
+
+        print("Cross-matching TGAS with {} for {}".format(catalog_name, cluster_name))
+        
+        catalog_short_name = catalog_name.split(".")[0]
+        
+        tgas_xmatch_sources = wsdb.retrieve_table(
+            """ WITH tgas AS (
+                    SELECT *
+                    FROM gaia_dr1.tgas_source
+                    WHERE q3c_radial_query(ra, dec, %(cluster_ra)s, %(cluster_dec)s, %(radius)s)
+                    )
+                SELECT * FROM tgas, {catalog_name} AS {catalog_short_name}
+                WHERE q3c_join(
+                    tgas.ra, tgas.dec, 
+                    {catalog_short_name}.ra, {catalog_short_name}.{catalog_dec}, 
+                    %(cone)s)
+            """.format(
+                catalog_name=catalog_name,
+                catalog_short_name=catalog_short_name,
+                catalog_dec="decl" if catalog_short_name == "twomass" else "dec"),
+            kwds)
+
+        tgas_xmatch_sources.write("{cluster_name}-tgas-{catalog_short_name}.csv"\
+            .format(cluster_name=cluster_name,
+                    catalog_short_name=catalog_short_name))
+
+        print("Found {N} TGAS-{catalog_short_name} sources for {cluster_name}"\
+            .format(N=len(tgas_xmatch_sources), 
+                    catalog_short_name=catalog_short_name,
+                    cluster_name=cluster_name))
---
+++ database.py
@@ -0,0 +1,161 @@
+
+""" A convenience object for databases. """
+
+import logging
+import numpy as np
+import psycopg2 as pg
+from astropy.table import Table
+from collections import Counter
+from decimal import Decimal
+from time import time
+
+logger = logging.getLogger("boris")
+
+
+class Database(object):
+
+    def __init__(self, **kwargs):
+        self.connection = pg.connect(**kwargs)
+        return None
+
+
+    def update(self, query, values=None, full_output=False, **kwargs):
+        """
+        Update the database with a SQL query.
+
+        :param query:
+            The SQL query to execute.
+
+        :type query:
+            str
+
+        :param values: [optional]
+            Values to use when formatting the SQL string.
+
+        :type values:
+            tuple or dict
+        """
+
+        logger.debug("Running SQL update query: {}".format(query))
+        names, results, cursor = self.execute(query, values, **kwargs)
+        return (names, results, cursor) if full_output else cursor.rowcount
+        
+
+    def retrieve(self, query, values=None, full_output=False, **kwargs):
+        """
+        Retrieve some data from the database.
+
+        :param query:
+            The SQL query to execute.
+
+        :type query:
+            str
+
+        :param values: [optional]
+            Values to use when formatting the SQL string.
+
+        :type values:
+            tuple or dict
+        """
+
+        names, results, cursor = self.execute(query, values, fetch=True,
+            **kwargs)
+        return (names, results, cursor.rowcount) if full_output else results
+
+
+    def execute(self, query, values=None, fetch=False, **kwargs):
+        """
+        Execute some SQL from the database.
+
+        :param query:
+            The SQL query to execute.
+
+        :type query:
+            str
+
+        :param values: [optional]
+            Values to use when formatting the SQL string.
+
+        :type values:
+            tuple or dict
+        """
+
+        t_init = time()
+        try:
+            with self.connection.cursor() as cursor:
+                cursor.execute(query, values)
+                if fetch: results = cursor.fetchall()
+                else: results = None
+
+        except pg.ProgrammingError:
+            logger.exception("SQL query failed: {0}, {1}".format(query, values))
+            cursor.close()
+            raise
+        
+        else:
+            taken = 1e3 * (time() - t_init)
+            try:
+                logger.debug("Took {0:.0f} ms for SQL query {1}".format(taken,
+                    " ".join((query % values).split())))
+            except (TypeError, ValueError):
+                logger.debug("Took {0:.0f} ms for SQL query {1} with values {2}"\
+                    .format(taken, query, values))
+        
+        names = None if cursor.description is None \
+            else tuple([column[0] for column in cursor.description])
+        return (names, results, cursor)
+
+
+    def retrieve_table(self, query, values=None, prefixes=True, **kwargs):
+        """
+        Retrieve a named table from a database.
+
+        :param query:
+            The SQL query to execute.
+
+        :type query:
+            str
+
+        :param values: [optional]
+            Values to use when formatting the SQL string.
+
+        :type values:
+            tuple or dict
+
+        :param prefixes: [optional]
+            Prefix duplicate column names with the given tuple.
+
+        :type prefixes:
+            tuple of str
+        """
+
+        names, rows, rowcount = self.retrieve(query, values, full_output=True)
+
+        # TODO:
+        if len(rows) == 0:
+            return None
+
+        counted_names = Counter(names)
+        duplicates = [k for k, v in counted_names.items() if v > 1]
+        if duplicates and prefixes:
+
+            use_prefixes = map(str, range(max(counted_names.values()))) \
+                if isinstance(prefixes, bool) else prefixes
+
+            # Put the prefixes and names in the right order & format for joining
+            prefixes = [
+                ([], [use_prefixes[names[:i].count(n)]])[n in duplicates] \
+                for i, n in enumerate(names)]
+            names = [[n] for n in names]
+            names = [".".join(p + n) for p, n in zip(prefixes, names)]
+
+        # Guess data types.
+        dtype = kwargs.pop("dtype", None)
+        if dtype is None:
+            dtype = []
+            for i, name in enumerate(names):
+                if isinstance(rows[0][i], Decimal):
+                    dtype.append(float)
+                else:
+                    dtype.append(type(rows[0][i]))
+        return Table(rows=rows, names=names, dtype=dtype)
